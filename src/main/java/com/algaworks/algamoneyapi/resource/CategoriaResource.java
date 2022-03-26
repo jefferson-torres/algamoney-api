9536be785b0f7388450faf99ade0c2a136b3ff6e
Jefferson Torres <jeferson.torres@hotmail.com>
2022-03-25 23:27:22 -0300

    3.6. Cadastrando nova categoria com POST

--- src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
@@ -1,11 +1,19 @@
 package com.algaworks.algamoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoneyapi.model.Categoria;
 import com.algaworks.algamoneyapi.repository.CategoriaRepository;
@@ -16,10 +24,29 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@GetMapping
-	public List<Categoria> listar(){
+	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
 	}
-	
+
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequestUri()
+				.path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo())
+				.toUri();
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findById(codigo).orElse(null);
+	}
+
 }
