6b987ea46dd811076de6a4dfebf52d92144a5495
Jefferson Torres <jeferson.torres@hotmail.com>
2022-03-27 00:02:38 -0300

    5.2. Desafio: Lista e busca de lançamentos

---
+++ src/main/java/com/algaworks/algamoneyapi/resource/LancamentoResource.java
@@ -0,0 +1,25 @@
+package com.algaworks.algamoneyapi.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algamoneyapi.model.Lancamento;
+import com.algaworks.algamoneyapi.repository.LacamentoRepository;
+
+@RestController
+@RequestMapping("/lancamentos")
+public class LancamentoResource {
+
+	@Autowired
+	private LacamentoRepository lacamentoRepository;
+	
+	@GetMapping
+	public List<Lancamento> listar(){
+		return lacamentoRepository.findAll();
+	}
+	
+}
